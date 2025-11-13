aa4226aa04d370f9eff6132fd8917db4b992e12e
Andrew Shilman <Andrew.Shilman@gmail.com>
2025-11-13 01:40:58 -0500

    Created initialized function in env.js to insert env values to key

--- src/core/env.js
+++ src/core/env.js
@@ -34,3 +34,28 @@ function getEnv(name) {
 
     return env;
 }
+
+
+//replace all {{KEY}} in the str with the values from env
+function interpolate(templateString, env = {}) {
+    if (typeof templateString !== 'string' || templateString.indexOf('{{') === -1) {
+        return templateString;
+    }
+
+    return templateString.replace(PLACEHOLDER_REGEX, (match, key) => {
+        if (Object.prototype.hasOwnProperty.call(env, key)) {
+            let value = env[key];
+            if (value === 'null') {
+                return '';
+            } else {
+                return String(value);
+            }
+        }
+
+        console.warn(`Warning: Missing environment value for "${key}".`);
+        return match;
+
+
+
+    }); 
+}
