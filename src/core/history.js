d221c511a86b915c9f154b84847ca123108a3281
Andrew Shilman <Andrew.Shilman@gmail.com>
2025-11-12 23:10:11 -0500

    Base template made for history.js. Handles command history and retrieval

---
+++ src/core/history.js
@@ -0,0 +1,16 @@
+//request history for user to see
+
+//just append to history.json with timestamp 
+function recordHistory(entry) {
+  throw new Error('Not implemented yet');
+}
+
+//to output record history when user wants to see
+function getHistory(options = {}) {
+  throw new Error('Not implemented yet');
+}
+
+module.exports = {
+  recordHistory,
+  getHistory
+};
