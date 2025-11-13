830e0e29b6c289bf5a8a88ef498fe30b63c9b12f
Andrew Shilman <Andrew.Shilman@gmail.com>
2025-11-12 23:08:42 -0500

    Base template for http.js. Handle the http requests and normalizing responses with axios

---
+++ src/core/http.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+//http request sending with axios
+async function sendRequest(config) {
+  throw new Error('Not implemented yet');
+}
+
+module.exports = {
+  sendRequest
+};
